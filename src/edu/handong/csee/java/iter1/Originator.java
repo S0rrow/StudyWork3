0f14288278153d0c098f5b1623d2f66dbff0b8eb
S0rrow <jae9hell@gmail.com>
2019-06-02 04:40:49 +0900

    built class files

--- src/edu/handong/csee/java/iter1/Originator.java
+++ src/edu/handong/csee/java/iter1/Originator.java
@@ -9,12 +9,12 @@ class Originator {
    /* lots of memory consumptive private data that is not necessary to define the
     * state and should thus not be saved. Hence the small memento object. */
     public void setState(Calendar date) {
-        System.out.println("Originator: Setting state to " + date);
+       // System.out.println("Originator: Setting state to " + date);
         this.date = date;
     }
 
     public Memento save() {
-        System.out.println("Originator: Saving to Memento.");
+        //System.out.println("Originator: Saving to Memento.");
         return new Memento(date);
     }
     
