9f6a43014ba82183f0717404f562b88fd0cb97cd
S0rrow <jae9hell@gmail.com>
2019-06-03 01:46:21 +0900

    minor modi

--- src/edu/handong/csee/java/iter1/Aggregate.java
+++ src/edu/handong/csee/java/iter1/Aggregate.java
@@ -3,5 +3,5 @@ package edu.handong.csee.java.iter1;
 import java.util.Iterator;
 
 public interface Aggregate {
-    public abstract Iterator iterator();
-   }
+	public abstract Iterator iterator();
+}
